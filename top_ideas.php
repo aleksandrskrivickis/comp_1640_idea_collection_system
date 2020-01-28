353841eff4563c5533b3abb1262a3fa008ff613b
Aleksandrs Krivickis <31312663+aleksandrskrivickis@users.noreply.github.com>
2020-01-28 17:19:48 +0000

    Create top_ideas.php

---
+++ top_ideas.php
@@ -0,0 +1,21 @@
+<?php
+#Include Decision function that checks if user is still logged in and dericts him to log in form, registration or main
+?>
+
+
+<html>
+<head>
+  <title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html>
+
+
+<?php
+include_once "./footer.php";
+?>
+
