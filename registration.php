6b525ab165f686c3285d6e77db91e98ebfe5dc80
Aleksandrs Krivickis <31312663+aleksandrskrivickis@users.noreply.github.com>
2020-01-28 17:17:35 +0000

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+
+<html>
+<head>
+  <title>Title of the document</title>
+</head>
+
+<body>
+The content of the document......
+</body>
+
+</html>
+
+
+<?php
+include_once "./footer.php";
+?>
