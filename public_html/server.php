5a08f85c9faf4c856d9357e870cb632ac0c33e2b
AR9915 <60543952+AR9915@users.noreply.github.com>
2020-02-17 22:26:56 +0000

    update for login page.

--- public_html/server.php
+++ public_html/server.php
@@ -1,12 +1,14 @@
 <?php
-    session_start();
+
+include('database.php');
+ session_start();
     
-    
+    $funObj = new Database();
     $logerrors = array();
     $errors = array();
 
 //connect to the database
- $db = mysqli_connect('mysql.cms.gre.ac.uk', 'st2645h', 'Enterprise94', 'mdb_st2645h');
+ //$db = mysqli_connect('mysql.cms.gre.ac.uk', 'st2645h', 'Enterprise94', 'mdb_st2645h');
 
 
 // if the register button is clicked
@@ -15,24 +17,20 @@
 if (isset($_POST['register_b'])){
    
     
-    $username = mysqli_real_escape_string($db,$_POST['username1']);
-    $email = mysqli_real_escape_string($db,$_POST['email1']);
-     $password1 = mysqli_real_escape_string($db,$_POST['password2']);
+    $username = $_POST['username1'];
+    $email = $_POST['email1'];
+     $password1 = $_POST['password2'];
     
    
     
     // ensure that form fields are filled properly
     
     
-    
-    if (!empty($username)){
+    $user = $funObj->usernameTaken($username);
+   if ($user){
         
-        $sql = "SELECT * FROM `User` WHERE `UserName` = '$username'";
-        $result = mysqli_query($db, $sql);
-            
-            if (mysqli_num_rows($result) == 1){
                 array_push($errors,"user already exists");
-            }
+            
     }
     
    if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
@@ -42,16 +40,20 @@ if (isset($_POST['register_b'])){
     // if there are no errors, save user to database
     
     if (count($errors) == 0) {
+         
         
-       
-        $sql = "INSERT INTO `User` (`UserName`, `Password`, `Email`) VALUES ('$username', '$password1', '$email')";
-        
-        mysqli_query($db, $sql);
-        $_SESSION['success'] = "you have successfully registered";
+         $register = $funObj->createUser($username, $password1, $email, 'Support', 'Computing');  
+        if($register){  
+                    array_push($errors,"Registration Successful");
+                }else{  
+                    array_push($errors,"Registration Not Successful");  
+                }  
        
     }
-    
 }
+    
+      
+
 
 
 // login //
@@ -61,22 +63,15 @@ if (isset($_POST['login_b'])){
     
    
     
-    $username = mysqli_real_escape_string($db,$_POST['username1']);
-     $password = mysqli_real_escape_string($db,$_POST['password']);
-    
-   
+    $username = $_POST['username1'];
+     $password = $_POST['password'];
     
     
-    $sql = "SELECT * FROM `User` WHERE `UserName` = '$username' AND `Password` = '$password'";
-    $result = mysqli_query($db, $sql);
-    
-     if (mysqli_num_rows($result) == 1){
-         
-        
-        $_SESSION['success'] = "you have successfully Logged IN";
-       
-     }
-     
+     $user = $funObj->checkLogin($username, $password);  
+        if ($user) {  
+            // login Success  
+          array_push($logerrors,"login successful"); 
+        } 
     else {
                 array_push($logerrors,"Wrong password or ID");
             }
