951c751f21a634c184c66d995a67ebafa4fedaad
sbtown <47975798+sbtown@users.noreply.github.com>
2020-02-28 12:39:20 +0000

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+
+//Gets session data then unsets the idNumber then sends to Login Form
+
+session_start();
+
+      
+//this will need to be set as a session when logging in 
+unset($_SESSION["username"]);
+
+header("Location:loginreg.php");
+?>
