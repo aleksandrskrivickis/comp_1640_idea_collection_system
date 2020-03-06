68a9244d9bbd35d918500da1a6f5acc6ff798523
sbtown <47975798+sbtown@users.noreply.github.com>
2020-03-06 13:24:45 +0000

    Add files via upload

--- public_html/submit_idea.php
+++ public_html/submit_idea.php
@@ -30,32 +30,16 @@
 
     <!--NAVIGATION BAR-->
     <!-- Image and text -->
-    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #036DA1;">
-        <a class="navbar-brand" href="#">Logo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <button class="btn btn-light my-2 my-sm-0" type="submit">Forums</button>
-                </li>
-            </ul>
+   
+ <div id="nav-placeholder">
 
-            <form class="form-inline my-2 my-lg-0">
-                <button style="margin:40px;" class="btn btn-light my-2 my-sm-0" type="Submit">Admin area</button>
+</div>
 
-            </form> 
-
-            <form class="form-inline my-2 my-lg-0">
-                <button class="btn btn-light my-2 my-sm-0" type="submit">Sign out</button>
-            </form>
-
-
-        </div>
-    </nav>
-       
+<script>
+$(function(){
+  $("#nav-placeholder").load("nav_bar.php");
+});
+</script>
     
     <!--PAGE TITLE/HEADER-->    
     <h1> General wellbeing forum</h1>
