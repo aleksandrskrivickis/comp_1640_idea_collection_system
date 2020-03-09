bf4f24496a68952b3973bbe8b962031af545b6c8
JamesDodd1 <48354221+JamesDodd1@users.noreply.github.com>
2020-03-09 05:22:50 +0000

    We are the Brother's Paradox.  And we like wearing women's frocks
    Adds file uploading

--- public_html/server.php
+++ public_html/server.php
@@ -1,78 +1,63 @@
 <?php
-
-include('database.php');
- session_start();
+    include_once('database.php');
+    session_start();
     
     $funObj = new Database();
     $logerrors = array();
     $errors = array();
-
-//connect to the database
- //$db = mysqli_connect('mysql.cms.gre.ac.uk', 'st2645h', 'Enterprise94', 'mdb_st2645h');
-
-
-// if the register button is clicked
-
-
-if (isset($_POST['register_b'])){
-   
-    
-    $username = $_POST['username1'];
-    $email = $_POST['email1'];
-     $password1 = $_POST['password2'];
-    
-   
-    
-    // ensure that form fields are filled properly
     
     
-    $user = $funObj->usernameTaken($username);
-   if ($user){
+    // if the register button is clicked
+
+    if (isset($_POST['register_b'])){
         
-                array_push($errors,"user already exists");
-            
+        $username = $_POST['username1'];
+        $email = $_POST['email1'];
+        $password1 = $_POST['password2'];
+        
+        
+        // ensure that form fields are filled properly
+
+
+        $user = $funObj->usernameTaken($username);
+        if ($user){
+            array_push($errors,"user already exists");
+        }
+
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
+            array_push($errors,"invalid email");
+        }
+
+        // if there are no errors, save user to database
+
+        if (count($errors) == 0) {
+
+            $register = $funObj->createUser($username, $password1, $email, 'Support', 'Computing');  
+
+            if($register){  
+                array_push($errors,"Registration Successful");
+            }else{  
+                array_push($errors,"Registration Not Successful");  
+            } 
+        }
     }
     
-   if(filter_var($email, FILTER_VALIDATE_EMAIL) == false){
-         array_push($errors,"invalid email");
- }
-   
-    // if there are no errors, save user to database
     
-    if (count($errors) == 0) {
-         
-        
-         $register = $funObj->createUser($username, $password1, $email, 'Support', 'Computing');  
-        if($register){  
-                    array_push($errors,"Registration Successful");
-                }else{  
-                    array_push($errors,"Registration Not Successful");  
-                }  
-       
-    }
-}
     
-      
+    
+    // login 
 
+    if (isset($_POST['login_b'])){
 
+        $username = $_POST['username1'];
+        $password = $_POST['password'];
 
-// login //
-
-
-if (isset($_POST['login_b'])){
-    
-   
-    
-    $username = $_POST['username1'];
-     $password = $_POST['password'];
-    
-    
-     $user = $funObj->checkLogin($username, $password);  
+        $user = $funObj->checkLogin($username, $password);  
         if ($user) {  
-           
+
             // login Success  
             array_push($logerrors,"login successful"); 
-            
+
             //session for logged in user
             session_start();
             $_SESSION['username'] = $username;
@@ -80,45 +65,34 @@ if (isset($_POST['login_b'])){
             $_SESSION['timeStamp'] = $time;
             // This will need to change to 
             header("Location: https://stuweb.cms.gre.ac.uk/~st2645h/forum.php");
-               
+
         } 
-    else {
-                array_push($logerrors,"Wrong password or ID");
-            }
-    
-    
-    
-    
-    
-
-}
-
-// submit idea
-
-if (isset($_POST['sub_idea'])){
-    
- // will be removed when all the pages are connected
-    $_SESSION['username'] = "IronSide";
-    $_SESSION['forum'] = "Computing";
-    
-    //initialise variables
-    $username = $_SESSION['username'];
-    $title = $_POST['ideatitle3'];
-    $category = [$_POST['category']];
-    $Description = $_POST['description'];
-    $anonymous = $_POST['ann'];
-    $forum = $_SESSION['forum'];
-   
-    
-    
-    //calling the function createIdea from the class database
-    $funObj->createIdea($Description, $title, $forum, $username, $anonymous, $category);
-    
-   
-     
-
-}
+        else {
+            array_push($logerrors,"Wrong password or ID");
+        }
+    }
 
 
 
+
+    // submit idea
+
+    if (isset($_POST['sub_idea'])){
+
+        // will be removed when all the pages are connected
+        $_SESSION['username'] = "IronSide";
+        $_SESSION['forum'] = "Computing";
+
+        //initialise variables
+        $username = $_SESSION['username'];
+        $title = $_POST['ideatitle3'];
+        $category = [$_POST['category']];
+        $description = $_POST['description'];
+        $anonymous = (is_null($_POST['ann'])) ? false : true;
+        $forum = $_SESSION['forum'];
+        $file = [$_FILES['fileUpload']];
+        
+        //calling the function createIdea from the class database
+        $funObj->createIdea($description, $title, $forum, $username, $anonymous, $category, $file);
+    }
 ?>
