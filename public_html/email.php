7cf9fd38ce239063d6b8fddff0ad005518bb90ea
sbtown <47975798+sbtown@users.noreply.github.com>
2020-03-22 12:54:11 +0000

    mail update

--- public_html/email.php
+++ public_html/email.php
@@ -3,21 +3,23 @@
 
 //when a user creates a new idea an email will be sent to the QAC
 //We will need to get the user department to send to ->getCoordinatorEmail
-
-
+include_once('database.php');
+session_start();
+//this will need to be taken out
+$_SESSION["department"] = "test";
+$department = $_SESSION["department"];
+ 
   $emailFunction = new Database();
 
     $emailQAM = $emailFunction->getCoordinatorEmail($department);  
 
 
-if($emailQAM->num_rows >= 0){
+if($emailQAM->num_rows > 0){
+    
     while($row = $emailQAM->fetch_assoc()){
+    
         //loops through emails of QAM from a department
         $email = $row["email"];
-    
-    
-
-
 $to = $email;
 //Email title
 $subject = "New Idea Submitted";
@@ -25,10 +27,10 @@ $subject = "New Idea Submitted";
 $txt = "A new idea has been submitted to your department"; 
 
 //who sent the message
-$headers = "From: DoNotReply@System.ac.uk\r\n";
-
+$headers = "From: DoNotReply@gre.ac.uk\r\n";
 
 mail("$to","$subject","$txt","$headers");
     }
 }
 ?>
+    
